15edc50ab0ef2f93e33a459b1754ff6327721573
Sebastian Dragan <sebastian.dragan305@gmail.com>
2023-10-24 22:45:51 +0200

    initial setting

---
+++ config/webpack.config.js
@@ -0,0 +1,61 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    main: './src/index.js',
+  },
+  output: {
+    filename: 'js/[name].js',
+    path: path.resolve(__dirname, '../', 'dist'),
+  },
+  devServer: {
+    open: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+      {
+        test: /\.(sass|scss)$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(jpg|png|svg|gif|jpeg)$/,
+        use: 'file-loader',
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        options: {
+          presets: [['@babel/preset-env', { useBuiltIns: 'usage' }]],
+          plugins: ['@babel/plugin-transform-class-properties'],
+        },
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: 'src/templates/template.html',
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+    }),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: 'src/assets/images',
+          to: 'images',
+        },
+      ],
+    }),
+  ],
+};
